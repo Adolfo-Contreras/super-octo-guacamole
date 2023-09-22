c235309eddc352273c12347da837d805fe46072e
Adolfo-Contreras <126830882+Adolfo-Contreras@users.noreply.github.com>
2023-09-22 09:43:12 -0600

    got rid of white space

--- node.js
+++ node.js
@@ -1,5 +1,4 @@
 //wrap everything in a .ready()
-
 $(document).ready(function(){
     // let count = 0;
     //     do{
@@ -7,17 +6,11 @@ $(document).ready(function(){
     //         $('#comments').removeClass('hidden')        
     //         count++
     //     }while (count<1)
-    
-    
-
-    
 // a function that lets you submit a comment that is then prepended
-
 $('#submitComment').on('click mouseenter mouseleave',function(e){
     if(e.type === 'click'){
     //comment variable
     let newComment = $("<div class='comment'><img src='abstract-user-flat-1.svg' alt='Abstract Profile of a Person' class='pfp'><div class='commentData'><h5 class='username'>Anonymous</h5><div class='editButton'>Edit</div><div class='deleteButton'>Delete</div> </div><div class='commentText'>    <p class='commentorText'> </p> </div> <div class='editing hidden'>     <input type='text' placeholder='Make an Edit' class='MakeEdit'>    <div class='submitEdit'>Submit</div> </div></div>");
-
     //check for commentors text
     let CommentInput = $('#commentInput').val();
     if(CommentInput){
@@ -33,7 +26,6 @@ $('#submitComment').on('click mouseenter mouseleave',function(e){
         alert('OI! MAKE A COMMENT >:(');
     }
     }
-
     //highlight submit button
     if(e.type === 'mouseenter'){
         $('#submitComment').toggleClass('buttonHighlight');
@@ -42,7 +34,6 @@ $('#submitComment').on('click mouseenter mouseleave',function(e){
         $('#submitComment').toggleClass('buttonHighlight');
     }
 });
-
 //the delete comment button also some dude on stack overflow recommended .closest and from my understanding it is going from the .deleteButton and finding the closest .comment which is its direct ancestor and yes i went to go find a post that can solve my problem and not just straight up ask
 $('.commentContainer').on('click mouseenter mouseleave','.deleteButton', function(event){
     if(event.type === 'click'){
@@ -55,7 +46,6 @@ $('.commentContainer').on('click mouseenter mouseleave','.deleteButton', functio
         $(this).closest('.deleteButton').toggleClass('buttonHighlight');
     }
 });
-
 //the edit button also i love the .siblings it made this so much easier thanks random stack overflow guy
 $('.commentContainer').on('click mouseenter mouseleave','.editButton', function(e){
     if(e.type === 'click'){
@@ -70,10 +60,7 @@ $('.commentContainer').on('click mouseenter mouseleave','.editButton', function(
     if(e.type === 'mouseleave'){
         $(this).closest('.editButton').toggleClass('buttonHighlight');
     }
-
-
 });
-
 //edit submit button
 $('.commentContainer').on('click mouseenter mouseleave','.submitEdit',function(e){
     if(e.type === 'click'){
@@ -92,6 +79,5 @@ $('.commentContainer').on('click mouseenter mouseleave','.submitEdit',function(e
     if(e.type === 'mouseleave'){
         $(this).closest('.submitEdit').toggleClass('buttonHighlight');
     }
-
 });
 }); ///END OF READY
